7c5386ff0fd8068cc52b4c4b4eb4a072b7bdbff2
MARVIN SILVA DE ALMEIDA <marvin.c2a@tjpa.jus.br>
2022-12-07 17:09:00 -0300

    implementação das funções de soma e subtração

---
+++ src/components/button/style.button.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const ButtonContainer = styled.button`
+    padding: 20px;
+    border: 1px solid #CDCDCD;
+    background-color: #000000;
+    color: #FFFF;
+    font-size: 24px;
+    font-weight: bold;
+    flex: 1;
+    &:hover{
+        opacity: 0.6;
+    }
+`
