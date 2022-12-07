9c6275b3201f74b7d72a503784d9640ca5d75b86
MARVIN SILVA DE ALMEIDA <marvin.c2a@tjpa.jus.br>
2022-12-07 12:12:14 -0300

    criado components de button e input

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Container } from "./App.styles";
+import Global from "./styles/global";
+
+const App = () => {
+  return (
+    <>
+      <Container>
+        <h2>calculadora</h2>
+      </Container>
+
+      <Global />
+    </>
+  );
+};
+
+export default App;
